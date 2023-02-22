6eea7ba84b3a15f0b568e0ddce3e767caa12ea3e
CheKaiWei <richardfeynman180778@gmail.com>
2023-02-22 10:04:14 +0800

    run

--- stage1_search.sh
+++ stage1_search.sh
@@ -1,4 +1,5 @@
-CUDA_VISIBLE_DEVICES=1 python train.py \
+CUDA_VISIBLE_DEVICES=0 python train.py \
 --mode super \
 --model_cfg experiments/supernet/supernet-Spikformer_small.yaml \
---epochs 500
+--resume 'logs/stage1_train_supernet/log_20230221_230318/model_best.pth.tar' \
+--epochs 1000
