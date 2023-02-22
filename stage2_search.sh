46879ea474955d7654ef6ce455f1b4ea59a61809
CheKaiWei <richardfeynman180778@gmail.com>
2023-02-22 17:48:15 +0800

    run

--- stage2_search.sh
+++ stage2_search.sh
@@ -1,4 +1,4 @@
 python search.py \
---model_cfg experiments/supernet/supernet-Spikformer.yaml \
- --resume logs/stage1_train_supernet/log_20230215_163857/checkpoint-501.pth.tar \
+--model_cfg experiments/supernet/supernet-Spikformer_small_model_1.yaml \
+ --resume /home/hanjing/CHE/Spikformer/logs/stage1_train_supernet/log_20230222_100148/model_best.pth.tar \
 --min-param-limits 1 --param-limits 100 
