03c1e44bbe0145de69433c959a7a1c76d01209ca
CheKaiWei <richardfeynman180778@gmail.com>
2023-03-13 09:51:40 +0000

    run

--- stage1.sh
+++ stage1.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=1 python train.py \
 --mode super \
---model_cfg experiments/supernet/supernet-Spikformer_small_model_4_3t_new.yaml \
---epochs 1000 \
---experiment_description "stage1: auto-spikformer CIFAR100 search 3t new"
+--model_cfg experiments/supernet/supernet-Spikformer_small_model_5_3tsmall_new.yaml \
+--epochs 2000 \
+--experiment_description "stage1: auto-spikformer CIFAR100 search 3t and small new"
